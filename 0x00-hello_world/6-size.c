5fab0d233c8b62e290e4728367f35c9fc625ff2c
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-06-15 02:42:41 -0700

    prints data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Return:Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf ("Size a char: %d byte(s)\n", sizeof(char));
 	printf ("Size an int: %d byte(s)\n", sizeof(int));
-	printf ("Size of a float: %d byte(s)\n", sizeof(float));
 	printf ("Size of a long int: %d byte(s)\n", sizeof(long));
 	printf ("Size of a long long int: %d byte(s)\n", sizeof(long long));
+	printf ("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
