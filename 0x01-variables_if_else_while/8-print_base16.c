8c65d46758672438d6878a06b372f68d54df8278
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-06-16 17:21:03 +0300

    prints hexadecimal in c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	while(m == 0 && m  < 10)
+	{
+		putchar(m);
+		m++;
+	}
+	while(m = 'a' && m < 'f')
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+
+	return (0);
+}
