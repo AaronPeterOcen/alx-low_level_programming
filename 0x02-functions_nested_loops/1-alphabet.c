115e2d8bacc5d9213d8a4b51662b5e08b6bbb8b4
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-06-20 12:19:17 +0300

    prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alpabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+int alphabet
