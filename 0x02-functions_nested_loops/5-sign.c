2d2f6603ae0de0cbf6b074947ada14b35ba246cc
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-06-20 18:42:36 +0300

    prints the sign of the numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		putchar('+');
 		return (1);
-		putchar('+' + n);
 	}
 	else if (n == 0)
 	{
+		putchar('0');
 		return (0);
-		putchar('0' + n);
 	}
 	else
 	{
+		putchar('-');
 		return (-1);
-		putchar('-' + n);
 	}
 }
