90f67ba4a7d486006b7519f76de1047cc606f697
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-06-21 18:46:34 +0300

    test for a postitive_or_negative() function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
- * main - tests function that prints sign of integers
- * positive_or_negative - testing the sign of a number
- *
- * Return: Always 0
- */
+* main - tests function that prints if integer is positive or negative
+*
+* positive_or_negative - finding the sign of a number
+* Return: 0
+*/
+
 int main(void)
 {
-	int n = 0;
+        int i;
 
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	} else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	} else if (n < 0)
-	{
-		printf("%d is negative \n", n);
-	}
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,10 +7,17 @@
 
 int main(void)
 {
-        int i;
-
-        i = 0;
-        positive_or_negative(i);
-
-        return (0);
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	return (0);
 }
