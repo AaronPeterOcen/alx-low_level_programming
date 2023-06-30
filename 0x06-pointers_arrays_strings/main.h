fe8ec204e5f8ccd05fc3c4f09904e0de9d2e624b
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-06-30 05:25:56 +0300

    function that prints an integer.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 int _strcmp(char *s1, char *s2);
 char *leet(char *);
+void print_number(int n);
+char *cap_string(char *);
 
 #endif
