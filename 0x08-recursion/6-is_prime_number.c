cbf0a5db2fc6515f414997c77221a53df6f671fb
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-07-06 20:09:33 +0300

    prints a string, followed by a new line.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,23 +8,21 @@
 int is_prime_number(int n)
 {
 	int i;
-
+	
 	if (n <= 1)
 	{
-		return (0);
+		return 0;
 	}
-	for (i = 2; i * i <= n; i++)
+
+	if (i == 1)
 	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
+		return 1;
 	}
-	/*else/
+
+	if (n % i == 0)
 	{
-		i = i - 1;
-		is_prime_number(n);
+		return 0;
 	}
-	*/
-	return (1);
+
+	return is_prime_number(n, i - 1);
 }
