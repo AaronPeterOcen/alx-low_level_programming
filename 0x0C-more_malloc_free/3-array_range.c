ed8c8b37d1cbdb70c3dd722fe09468f68f29fed0
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-07-13 14:50:09 +0300

    function that allocates memory using malloc.

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: minimum value of the array
+ * @max: max value of the arrat
+ */
