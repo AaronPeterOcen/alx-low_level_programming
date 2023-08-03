fd9252f0228d5cabbd0e5e241fc30a11b4a19a53
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-08-03 12:52:25 +0300

    fix for task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int sln = 0;
 	int pwr = 1;
-	int i = strlen(b) -1;
+	int i = strlen(b) - 1;
 
 	if (b == NULL)
 	{
@@ -20,7 +20,7 @@ unsigned int binary_to_uint(const char *b)
 	while (i >= 0)
 	{
 		if (b[i] != '0' && b[i] != '1')
-			return(0);
+			return (0);
 		sln += ((b[i] - '0') * pwr);
 		pwr *= 2;
 		i--;
