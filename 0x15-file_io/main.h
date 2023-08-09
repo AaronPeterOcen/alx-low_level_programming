4f0e7564d83ca2b559b2ff82b08b4eb2400aa6df
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-08-09 18:18:50 +0300

    number 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define MAXISZE 1024
+
 int _putchar(char c);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
