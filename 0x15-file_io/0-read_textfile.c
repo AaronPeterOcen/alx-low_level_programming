dfe1e314bdb9bfb1ec6bb72a816bffa0c0959b04
Pteer <pteer@pop-os.localdomain>
2023-09-04 11:26:41 +0300

    function files for the project

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX stdout
+ * @filename: ...pointer to the file name
+ * @letters: ...letters read and printed
+ *
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f_desc;
+	char *buffer;
+	ssize_t nd, nwr;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	
+	f_desc = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (buffer == NULL)
+		return (0);
+	nd = read(f_desc, buffer, letters);
+	nwr = write(STDOUT_FILENO, buffer, nd);
+
+	close(f_desc);
+
+	free(buffer);
+
+	return (nwr);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,87 @@
+#include "main.h"
+#define MAXSIZE 1024
+
+/**
+ * print_exit - ...
+ * @e: ...
+ * @s: ...
+ * @fdesc: ...
+ * Return: 0
+ */
+int print_exit(int e, char *s, int fdesc)
+{
+	if (e == 97)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(e);
+	}
+	else if (e == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+		exit(e);
+	}
+	else if (e == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+		exit(e);
+	}
+	else if (e == 100)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdesc);
+		exit(e);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * main - copies the content fo a file to another
+ * @argc: arg count
+ * @argv: array of pointers
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int file_in, file_out, rd_stat, wr_stat, shut_in, shut_out;
+	char buffer[MAXSIZE];
+
+	if (argc != 3)
+	{
+		print_exit(97, NULL, 0);
+	}
+	file_in = open(argv[1],  O_RDONLY);
+	if (file_in == -1)
+	{
+		print_exit(98, argv[1], 0);
+	}
+	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_out == -1)
+	{
+		print_exit(99, argv[2], 0);
+	}
+
+	for (; (rd_stat = read(file_in, buffer, MAXSIZE)) != 0;)
+	{
+		if (rd_stat == -1)
+		{
+			print_exit(98, argv[1], 0);
+		}
+		wr_stat = write(file_out, buffer, rd_stat);
+		if (wr_stat == -1)
+		{
+			print_exit(99, argv[2], 0);
+		}
+	}
+	shut_in = close(file_in);
+	if (shut_in == -1)
+	{
+		print_exit(100, NULL, file_in);
+	}
+	shut_out = close(file_out);
+	if (shut_out == -1)
+		print_exit(100, NULL, file_out);
+
+	return (0);
+}
