068f87a99cbf76dbc7f6fca62e0f195ae5967b1e
Pteer <pteer@pop-os.localdomain>
2023-09-04 12:11:34 +0300

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	
 	f_desc = open(filename, O_RDONLY);
 
-	if (fd == -1)
+	if (f_desc == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * (letters));
