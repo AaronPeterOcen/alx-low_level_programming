e5db5d49d7be49746074fd18cd7c3f5e8d382585
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-10-13 14:29:17 +0300

    task 3 fix

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,7 +12,7 @@
  *
  */
 
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *nn = malloc(sizeof(dlistint_t));
 
